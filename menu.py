9edd63ddc85602ff388344725904a5b0363094c3
Uwe Ziegenhagen <UweZiegenhagen@users.noreply.github.com>
2020-03-23 13:09:16 +0100

    Initial commit

---
+++ menu.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 20200323
+
+@author: Uwe Ziegenhagen, ziegenhagen@gmail.com
+"""
+
+
+from tkinter import *
+
+
+
+class Application(Frame):
+    def say_hi(self):
+        print("hi there, everyone!")
+
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"]   = "red"
+        self.QUIT["command"] =  self.quit
+
+        self.QUIT.pack({"side": "left"})
+
+        self.hi_there = Button(self)
+        self.hi_there["text"] = "Hello",
+        self.hi_there["command"] = self.say_hi
+
+        self.hi_there.pack({"side": "left"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+root = Tk()
+root.geometry("800x480")
+app = Application(master=root)
+app.mainloop()
+root.destroy()
